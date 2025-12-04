3e253b170259328a53e9eda70957429517e5c55a
HuanCanhCut <tronghuanxxx@gmail.com>
2025-12-04 11:06:50 +0000

    chore: update Next.js to v15.5.7 and enable experimental typed routes in configuration

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,11 @@ const nextConfig = {
             },
         ],
     },
+    typedRoutes: {
+        experimental: {
+            typedRoutes: true,
+        },
+    },
 }
 
 export default nextConfig
